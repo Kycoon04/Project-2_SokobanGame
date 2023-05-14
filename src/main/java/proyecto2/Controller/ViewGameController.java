e53699c779dcc301b372c6e279608dc65ccb9669
Kycoon <jomaval4@gmail.com>
2023-05-13 23:20:27 -0600

    cambio

--- src/main/java/proyecto2/Controller/ViewGameController.java
+++ src/main/java/proyecto2/Controller/ViewGameController.java
@@ -11,19 +11,24 @@ import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class ViewGameController implements Initializable {
-
-    private String[] MatrizNumber;
+    ImageView Respaldo = new ImageView(new Image("/proyecto2/Assets/BaseTierra.png"));
+    private String[][] MatrizNumber = new String[10][10];
+    String[] numeros;
     private int PJ_X;
     private int PJ_Y;
+    private int numRows;
+    private int numCols;
 
     @FXML
     private GridPane Fisic;
@@ -36,23 +41,68 @@ public class ViewGameController implements Initializable {
         Platform.runLater(() -> {
             Scene scene = root.getScene();
             scene.setOnKeyPressed((KeyEvent event) -> {
-                if (event.getCode() == KeyCode.UP) {
-                    System.out.println("Se presionó la tecla hacia arriba");
-                }
-                if (event.getCode() == KeyCode.DOWN) {
-                    move(event);
-                }
+                move(event);
             });
         });
     }
 
     public void move(KeyEvent event) {
-        ImageView imageView = new ImageView(new Image("/proyecto2/Assets/Personaje.png"));
-        ImageView imageView2 = new ImageView(new Image("/proyecto2/Assets/BaseTierra.png"));
-        if (event.getCode() == KeyCode.DOWN) {
-            Fisic.add(imageView2,PJ_X, PJ_Y);
-            Fisic.add(imageView, PJ_X, PJ_Y+1);
-            PJ_Y+=1;
+        ImageView PersonajeMove = new ImageView(new Image("/proyecto2/Assets/Personaje.png"));
+
+
+        switch (event.getCode()) {
+            case UP:
+                if (!MatrizNumber[PJ_Y - 1][PJ_X].equals("1")) {
+                    Fisic.add(PersonajeMove, PJ_X, PJ_Y - 1);
+                    PJ_Y--;
+                }
+                break;
+            case DOWN:
+                if (!MatrizNumber[PJ_Y + 1][PJ_X].equals("1")) {
+                    Fisic.add(PersonajeMove, PJ_X, PJ_Y + 1);
+                    PJ_Y++;
+                }
+                break;
+            case LEFT:
+                if (!MatrizNumber[PJ_Y][PJ_X - 1].equals("1")) {
+                    Fisic.add(PersonajeMove, PJ_X - 1, PJ_Y);
+                    PJ_X--;
+                }
+                break;
+            case RIGHT:
+                if (!MatrizNumber[PJ_Y][PJ_X + 1].equals("1")) {
+                    Fisic.add(PersonajeMove, PJ_X + 1, PJ_Y);
+                    PJ_X++;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public ImageView SiguienteRespaldo(int row, int col) {
+        switch (MatrizNumber[row][col]) {
+            case "0":
+                return new ImageView(new Image("/proyecto2/Assets/BaseTierra.png"));
+            case "3":
+                return new ImageView(new Image("/proyecto2/Assets/BloqueDestino.png"));
+        }
+        return null;
+    }
+
+    public void CargarMatriz(String[] numeros) {
+        int index = 0;
+        for (int i = 0; i < MatrizNumber.length; i++) {
+            for (int j = 0; j < MatrizNumber.length; j++) {
+                MatrizNumber[i][j] = numeros[index];
+                index++;
+            }
+        }
+        for (int i = 0; i < MatrizNumber.length; i++) {
+            for (int j = 0; j < MatrizNumber.length; j++) {
+                System.out.print(MatrizNumber[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
@@ -73,17 +123,18 @@ public class ViewGameController implements Initializable {
             e.printStackTrace();
         }
         String file = builder.toString();
-        MatrizNumber = file.split("\\s+");
+        numeros = file.split("\\s+");
+        CargarMatriz(numeros);
 
-        int numRows = Fisic.getRowCount();
-        int numCols = Fisic.getColumnCount();
+        numRows = Fisic.getRowCount();
+        numCols = Fisic.getColumnCount();
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                switch (MatrizNumber[i]) {
+                switch (MatrizNumber[row][col]) {
                     case "0":
-                       imageView = new ImageView(new Image("/proyecto2/Assets/BaseTierra.png"));
-                       Fisic.add(imageView, col, row);
-                       break;
+                        imageView = new ImageView(new Image("/proyecto2/Assets/BaseTierra.png"));
+                        Fisic.add(imageView, col, row);
+                        break;
                     case "1":
                         imageView = new ImageView(new Image("/proyecto2/Assets/BloqueBloqueo.png"));
                         Fisic.add(imageView, col, row);
@@ -97,7 +148,7 @@ public class ViewGameController implements Initializable {
                         Fisic.add(imageView, col, row);
                         break;
                     case "4":
-                        imageView = new ImageView(new Image("/proyecto2/Assets/Personaje.png"));
+                        imageView = new ImageView(new Image("/proyecto2/Assets/Personaje2.png"));
                         Fisic.add(imageView, col, row);
                         PJ_X = col;
                         PJ_Y = row;
